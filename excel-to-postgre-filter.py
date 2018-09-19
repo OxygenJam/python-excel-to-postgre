655c76068ab56dd102444dbe20a4ae29661df423
OxygenJam <triztandriz@gmail.com>
2018-09-19 17:50:57 +0800

    added another script for filters

---
+++ excel-to-postgre-filter.py
@@ -0,0 +1,340 @@
+# This automatically inserts data from an excel
+# sheet to a specified Postgre DB table
+# made by the table-generator.py script
+# -Zird Triztan E. Driz
+
+import sys
+import datetime
+import re
+import os
+
+# Core package dependencies
+import xlrd
+import psycopg2
+
+# Package for loading local .env files
+from dotenv import load_dotenv
+
+#Package for UI Color
+from colorama import init
+from termcolor import colored
+
+# Use Colorama to make Termcolor work on Windows too
+init()
+
+# Loads the env file
+load_dotenv()
+
+# Colored printing
+def logPrint(word):
+    sys.stdout.write(colored(">> ","green"))
+    sys.stdout.write(word)
+    sys.stdout.flush()
+    
+def errPrint(word):
+    sys.stdout.write(colored("ERR: ","red"))
+    sys.stdout.flush()
+    print(word)
+
+def gPrint(word):
+    print(colored(word,"green"))
+
+def rPrint(word):
+    print(colored(word,"red"))
+
+def bPrint(word):
+    print(colored(word,"blue"))
+
+# Line break function
+def lineBreak():
+    print_break = "============================="
+    print(colored(f"\n{print_break}\n", "green"))
+
+# Checks that syntax or arguments have been met
+def isSyntaxCorrect(arr):
+
+    if (len(arr) < 5):
+        return -1
+
+    return 0
+
+# Checks the data type
+def isIntOrFloat(sheet_val):
+
+    # Check if string
+    if(not isinstance(sheet_val, str)):
+
+        # Check if float
+        if((sheet_val % 1) == 0.0):
+            return int(sheet_val)
+        else:
+            return float(sheet_val)
+    else:
+            new_val = str(sheet_val)
+
+            # Check for apostrophes remove quotations
+            new_val = new_val.replace("'","''")
+            new_val = new_val.replace('"',"")
+            return str(new_val)
+
+# Removes special characters and formats column name
+def formatColumnNames(col_name):
+    col_name = re.sub(r"[/() -]","_",col_name)
+    col_name = re.sub(r"[.,]","",col_name)
+    col_name = re.sub(r"_(\w+)\1+","_",col_name)
+        
+    col_name = col_name.replace("%","pct")
+    col_name = col_name.replace("#","nbr")
+    col_name = col_name.replace("&","and")
+
+    col_name = str.lower(col_name)
+
+    return col_name
+
+
+# Loads the sheet where the list of filtered columns
+def loadSheetOfFilteredColumns(file_name):
+
+    filters = []
+
+    with open("../Data/Filters/" + file_name  + ".txt") as file:
+
+        data = file.readlines()
+
+        for line in data:
+
+            column = formatColumnNames(line.strip())
+
+            filters.append(column)
+
+    #book = xlrd.open_workbook("../Data/Filters" + file_name)
+    #sheet = book.sheet_by_index(0)
+    
+    # List of columns from excel file B in store in 1st column
+    # To be used as filter for excel file A
+    #for r in range(0,len(sheet.nrows)):
+
+    #    column = str(sheet.cell(r,0).value)
+
+    #    column = formatColumnNames(column)
+
+    #    filters.append(column)
+
+    return filters
+
+# Main Function
+def main():
+    start = datetime.datetime.now()
+    args = sys.argv
+
+    if(isSyntaxCorrect(args[1:])==-1):
+        errPrint("Please indicate the <filename> and <sheetname> in your argument\n")
+
+        rPrint("e.g.\n")
+        rPrint("python table-generator.py <some-file> <some-sheet> <table-name> <filter-filename> <where-clause>")
+        
+        logPrint("")
+        args.append(str(input("Please provide the name of the excel file: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the sheet name of the excel file: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the name of the table to be inserted to: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the filename (.txt) of the column filter to be used: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the where clause (col_name = val): ")))
+
+    logPrint("Arguments: \n\n")
+    bPrint(str(args) + "\n")
+
+    srcFileName = args[1] + ".xlsx"
+    srcSheetName = args[2]
+    tableName = args[3]
+
+    filters = str(args[4])
+    filters = loadSheetOfFilteredColumns(filters)
+
+    whereClause = str(args[5])
+
+    if(whereClause.find(" = ") == -1):
+        whereClause = whereClause.replace("=", " = ")
+
+    whereClause = whereClause.split(" = ")
+    whereClause[0] = formatColumnNames(whereClause[0])
+
+    logPrint(f"WHERE {whereClause[0]} == {whereClause[1]}\n")
+    logPrint("Filters: \n\n ")
+    print(colored(str(filters) + "\n", "green"))
+
+    logPrint("Loading the worksheet...\n")
+   
+    # Calls the psycopg factory function to return a Connection class instance
+    try:
+
+        book = xlrd.open_workbook("../Data/" + srcFileName)
+        sheet = book.sheet_by_name(srcSheetName)
+        logPrint("Finished loading the worksheet!\n")
+
+        db = psycopg2.connect (database = os.getenv("db"), user=os.getenv("user"), password=os.getenv("password"),host=os.getenv("host"),port=os.getenv("port"))
+        cursor = db.cursor()
+    except Exception as e:
+
+        lineBreak()
+        errPrint(str(e) +"\n")
+        lineBreak()
+        
+        return -1
+
+    logPrint("Connected to the ")
+    bPrint(f"{os.getenv('db')} database")
+    lineBreak()
+    logPrint("Preparing query...\n")
+    
+    # Truncating of table
+    query = "TRUNCATE {}".format(tableName)
+
+    try:
+
+        cursor.execute(query)
+    except Exception as e:
+
+        lineBreak()
+        errPrint(str(e) +"\n")
+        lineBreak()
+        
+        return -1
+
+    # Query pre-processing
+
+    # Take note table name must be same as sheetname
+    query = "INSERT INTO {} VALUES".format(tableName)
+    query += " {}"
+
+    for c in range(0, sheet.ncols):
+        col = sheet.cell(0,c).value
+        col = formatColumnNames(col)
+
+        if(col == whereClause[0]):
+            whereClause.append(c)
+            break
+
+    logPrint("Finished preparing query\n")
+    lineBreak()
+    print("QUERY STRING: \n\n")
+    print(colored(query,'green'))
+
+    data_inserted = 0
+    with db.cursor() as cursor:
+
+        for r in range(1,sheet.nrows):
+            arr = []
+
+            # If column is the where clause
+            # and value is equal to where value
+            if(str(sheet.cell(r,whereClause[2]).value) != whereClause[1]):
+                continue
+
+            # Formatting info is not implemented in .xlsx
+            #print(f"Row # {r} is {sheet.rowinfo_map[]}")
+
+            for c in range(0,sheet.ncols):
+                col = sheet.cell(0,c).value
+                col = formatColumnNames(col)
+
+                # Accept only columns in filter
+                if(col in filters):
+
+                    val = sheet.cell(r,c).value
+
+                    if(sheet.cell_type(r,c) == 0):
+                        
+                        arr.append("NULL")
+                    else:
+
+                        if(sheet.cell_type(r,c) != 3):
+
+                            arr.append(isIntOrFloat(val))
+                        else:
+                            arr.append(str(xlrd.xldate.xldate_as_datetime(val, book.datemode)))
+                else:
+                    
+                    continue
+            
+            arr.append(str(datetime.datetime.now().strftime("%Y-%m-%d")))
+            arr.append(srcFileName)
+
+            values = tuple(arr)
+            
+            formatted_query = query.format(values)
+            # Format any NULL values
+            formatted_query = formatted_query.replace("'NULL'","NULL")
+            formatted_query = formatted_query.replace('"',"'")
+
+            try:
+
+                data_inserted += 1
+                cursor.execute(formatted_query)
+            except Exception as e:
+
+                lineBreak()
+                errPrint(str(e) + "\n")
+                lineBreak()
+                print("Error on excel data row {}\n".format(r+1))
+                print("Tuple data: \n {}\n".format(values))
+                print("Query data: \n {}\n".format(formatted_query))
+
+                end = datetime.datetime.now()
+
+                
+                elapsed = end - start
+                elapsed_mileseconds = elapsed.microseconds//1000
+                elapsed = list(divmod(elapsed.days * 86400 + elapsed.seconds, 60))
+                elapsed.append(elapsed_mileseconds)
+
+                logPrint(f"Time elapsed: {elapsed[0]} minutes, {elapsed[1]} seconds, and {elapsed[2]} milleseconds")
+                lineBreak()
+                            
+                return -1
+
+    try:   
+
+        db.commit()
+        db.close()
+    except Exception as e:
+
+        lineBreak()
+        errPrint(str(e) +"\n")
+        lineBreak()
+        
+        return -1
+
+    lineBreak()
+
+    logPrint(str(data_inserted) + " number of rows / or data inserted to table\n")
+    logPrint(str(len(filters) + 2) + " number of columns / number of data per row\n\n")
+
+    end = datetime.datetime.now()
+
+    elapsed = end - start
+    elapsed_mileseconds = elapsed.microseconds//1000
+    elapsed = list(divmod(elapsed.days * 86400 + elapsed.seconds, 60))
+    elapsed.append(elapsed_mileseconds)
+
+    logPrint(f"Time elapsed: {elapsed[0]} minutes, {elapsed[1]} seconds, and {elapsed[2]} milleseconds")
+    lineBreak()
+    
+    return 0
+
+if __name__ == '__main__':
+    
+    code = main()
+
+    if(code==0):
+        print("Script executed with no errors...")
+    else:
+        print("Script exited with an error...")
+    input("PRESS ENTER TO EXIT")
---
+++ table-generator-filter.py
@@ -0,0 +1,273 @@
+# This automatically generate a table to a Postgre
+# table from an Excel file as a data source.
+# This also appends two extra columns at the 
+# end containing the date of file uploaded
+# and the name of the file uploaded
+#
+# Partner file to this script
+# is the excel-to-postgre .py files
+# -Zird Triztan E. Driz
+
+import sys
+import re
+import os
+import datetime
+
+# Core package dependencies
+import xlrd
+import psycopg2
+
+# Package for loading local .env files
+from dotenv import load_dotenv
+
+#Package for UI Color
+from colorama import init
+from termcolor import colored
+
+# Use Colorama to make Termcolor work on Windows too
+init()
+
+# Loads the env file
+load_dotenv()
+
+# Colored printing
+def logPrint(word):
+    sys.stdout.write(colored(">> ","green"))
+    sys.stdout.write(word)
+    sys.stdout.flush()
+    
+def errPrint(word):
+    sys.stdout.write(colored("ERR: ","red"))
+    sys.stdout.flush()
+    print(word)
+
+def gPrint(word):
+    print(colored(word,"green"))
+
+def rPrint(word):
+    print(colored(word,"red"))
+
+def bPrint(word):
+    print(colored(word,"blue"))
+
+# Line break function
+def lineBreak():
+    print_break = "============================="
+    print(colored(f"\n{print_break}\n", "green"))
+
+# Checks that syntax or arguments have been met
+def isSyntaxCorrect(arr):
+
+    if (len(arr) < 5):
+        return -1
+
+    return 0
+
+# Checks the data type
+def isIntOrFloat(sheet_val):
+
+    # check if string
+    if(not isinstance(sheet_val, str)):
+
+        return "NUMERIC"
+    else:
+            return "VARCHAR"
+
+# Removes special characters and formats column name
+def formatColumnNames(col_name):
+    col_name = re.sub(r"[/() -]","_",col_name)
+    col_name = re.sub(r"[.,]","",col_name)
+    col_name = re.sub(r"_(\w+)\1+","_",col_name)
+        
+    col_name = col_name.replace("%","pct")
+    col_name = col_name.replace("#","nbr")
+    col_name = col_name.replace("&","and")
+
+    col_name = str.lower(col_name)
+
+    return col_name
+
+# Loads the sheet where the list of filtered columns
+def loadSheetOfFilteredColumns(file_name):
+
+    filters = []
+
+    with open("../Data/Filters/" + file_name  + ".txt") as file:
+
+        data = file.readlines()
+
+        for line in data:
+
+            column = formatColumnNames(line.strip())
+
+            filters.append(column)
+
+    #book = xlrd.open_workbook("../Data/Filters" + file_name)
+    #sheet = book.sheet_by_index(0)
+    
+    # List of columns from excel file B in store in 1st column
+    # To be used as filter for excel file A
+    #for r in range(0,len(sheet.nrows)):
+
+    #    column = str(sheet.cell(r,0).value)
+
+    #    column = formatColumnNames(column)
+
+    #    filters.append(column)
+
+    return filters
+
+# Main Function
+def main():
+    start = datetime.datetime.now()
+    args = sys.argv
+
+    if(isSyntaxCorrect(args[1:])==-1):
+        
+        errPrint("Please indicate the <filename> and <sheetname> in your argument\n")
+
+        rPrint("e.g.\n")
+        rPrint("python table-generator.py <some-file> <some-sheet> <table-name> <row-to-compare>")
+        
+        logPrint("")
+        args.append(str(input("Please provide the name of the excel file: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the sheet name of the excel file: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the name of the table to be inserted to: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the filename (.txt) of the column filter to be used: ")))
+
+        logPrint("")
+        args.append(str(input("Please provide the row number(+1 from the column header) to be based in creating the table: ")))
+    
+    logPrint("Arguments: \n\n")
+    bPrint(str(args) + "\n")
+
+    srcFileName = args[1] + ".xlsx"
+    srcSheetName = args[2]
+    tableName = args[3]
+
+    filters = str(args[4])
+    filters = loadSheetOfFilteredColumns(filters)
+    
+    rowToCompare = int(args[5])
+
+    logPrint("Filters: \n " + str(filters) + "\n")
+    logPrint("Loading the worksheet...\n")
+    
+    query = "CREATE TABLE {}(".format(tableName)
+
+    try:
+
+        book = xlrd.open_workbook("../Data/" + srcFileName)
+        sheet = book.sheet_by_name(srcSheetName)
+        logPrint("Finished loading the worksheet!\n")
+
+        db = psycopg2.connect (database = os.getenv("db"), user=os.getenv("user"), password=os.getenv("password"),host=os.getenv("host"),port=os.getenv("port"))
+        cursor = db.cursor()
+    except Exception as e:
+
+        lineBreak()
+        errPrint(str(e) +"\n")
+        lineBreak()
+        
+        return -1
+
+    logPrint("Connected to the ")
+    bPrint(f"{os.getenv('db')} database")
+    lineBreak()
+    logPrint("Preparing query...\n")
+
+    c = 0
+    c_length = sheet.ncols
+
+    while(c<c_length):
+
+        # Special character cleansing
+        column = str(sheet.cell(0,c).value)
+
+        # Replaces space, forward slash, parantheses, 
+        # and dash with a single underscore
+        # also removes periods and commas
+        column = formatColumnNames(column)
+
+        if(sheet.cell_type(1,c)!= 3):
+            columntype = isIntOrFloat(sheet.cell(rowToCompare,c).value)
+        else:
+            columntype = "VARCHAR"
+
+        if(column in filters):
+            logPrint(f"Column #  {c+1} :")
+            bPrint(f"{sheet.cell(0,c).value}")
+            query += "{} {},".format(column, columntype) 
+
+        c += 1
+
+    query += "date_file_uploaded VARCHAR, file_name VARCHAR)"
+    logPrint("Finished preparing query\n")
+    lineBreak()
+    print("QUERY STRING: \n\n")
+    print(colored(query,'green'))
+
+    try:
+
+        cursor.execute(query)
+    except Exception as e:
+
+        lineBreak()
+        errPrint(str(e) + "\n")
+
+        end = datetime.datetime.now()
+
+
+        elapsed = end - start
+        elapsed_mileseconds = elapsed.microseconds//1000
+        elapsed = list(divmod(elapsed.days * 86400 + elapsed.seconds, 60))
+        elapsed.append(elapsed_mileseconds)
+
+        logPrint(f"Time elapsed: {elapsed[0]} minutes, {elapsed[1]} seconds, and {elapsed[2]} milleseconds")
+        lineBreak()
+
+        return -1
+
+    try:
+
+        cursor.close()
+
+        db.commit()
+        db.close()
+    except Exception as e:
+
+        lineBreak()
+        errPrint(str(e) +"\n")
+        lineBreak()
+        
+        return -1
+
+    lineBreak()
+
+    end = datetime.datetime.now()
+
+    elapsed = end - start
+    elapsed_mileseconds = elapsed.microseconds//1000
+    elapsed = list(divmod(elapsed.days * 86400 + elapsed.seconds, 60))
+    elapsed.append(elapsed_mileseconds)
+
+    logPrint(f"Time elapsed: {elapsed[0]} minutes, {elapsed[1]} seconds, and {elapsed[2]} milleseconds")
+    lineBreak()
+
+    return 0
+
+if __name__ == '__main__':
+
+    code = main()
+
+    if(code==0):
+        print("Script executed with no errors...")
+    else:
+        print("Script exited with an error...")
+    input("PRESS ENTER TO EXIT")
